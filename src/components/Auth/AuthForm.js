ea030f46efff28cfb92a193ca3390e30965e3151
Safwan Backar <safwanbackar29@gmail.com>
2022-07-20 16:29:22 +0530

    change password function

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,60 +1,71 @@
-import { useRef, useState } from 'react';
+import { useState, useRef, useContext } from 'react';
+import AuthContext from '../../store/auth-context';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
-  const emailInputRef = useRef()
-  const passwordInputRef = useRef()
-
+  const authCtx = useContext(AuthContext)
 
   const [isLogin, setIsLogin] = useState(true);
-  const [isLoading, setIsLoading] = useState(false)  
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
 
-  const submitHandler = (event) =>{
-    event.preventDefault()
-    const enteredEmail = emailInputRef.current.value
-    const enteredPassword = passwordInputRef.current.value
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
 
-    setIsLoading(true)
-    if(isLogin){
-    }else{
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCldbU0qlN1v0MwhqoeS8coleBlPvG8aDw',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: enteredEmail,
-          password: enteredPassword,
-          returnSecureToken: true
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }
-      ).then(res =>{
-        setIsLoading(false)
-        if(res.ok){
+    // optional: Add validation
 
-        }else{
-          return res.json().then(data =>{
-            let errorMessage = 'Authentication failed!'
-            // if(data && data.error && data.error.message){
-            //   errorMessage = data.error.message
+    setIsLoading(true);
+    let url;
+    if (isLogin) {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCldbU0qlN1v0MwhqoeS8coleBlPvG8aDw';
+    } else {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCldbU0qlN1v0MwhqoeS8coleBlPvG8aDw';
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = 'Authentication failed!';
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
             // }
-            alert(errorMessage)
-          })
+
+            throw new Error(errorMessage);
+          });
         }
       })
-    }
-
-
-  }
-
+      .then((data) => {
+        authCtx.login(data.idToken)
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
 
   return (
     <section className={classes.auth}>
@@ -66,10 +77,17 @@ const AuthForm = () => {
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required ref={passwordInputRef} />
+          <input
+            type='password'
+            id='password'
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {!isLoading && (
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          )}
           {isLoading && <p>Sending request...</p>}
           <button
             type='button'
